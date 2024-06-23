abcc41ab9b03f81fe588590fe31330d0dcba0a57
PatrickMamsery <pkrobert1612@gmail.com>
2024-06-23 19:02:38 +0300

    hotfix: namespace errors

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\Api\ReportController;
+use App\Http\Controllers\API\ReportController;
 use App\Http\Controllers\API\PropertyController;
 
 /*
