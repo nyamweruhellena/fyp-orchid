a7c11bbe4b7e2a8861efaf646e6da8c42594f228
nyamweruhellena <hellena0405@gmail.com>
2024-06-25 19:49:40 +0300

    fix: add serial number

--- app/Http/Resources/ReportResource.php
+++ app/Http/Resources/ReportResource.php
@@ -18,6 +18,7 @@ class ReportResource extends JsonResource
             'id' => $this->id,
             'property_name' => $this->property->name,
             'property_location' => $this->property->collegeBlock->name,
+            'serial_number' => $this->property->serial_no,
             'description' => $this->description,
             'reported_on' => $this->created_at,
             'status' => $this->status,
